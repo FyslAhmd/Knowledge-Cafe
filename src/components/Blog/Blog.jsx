73708f90cd4ac0fb1e7b67acbc1c59e201baf61f
Faysal Ahmed <info.faysal.32@gmail.com>
2025-04-08 00:40:04 +0600

    first commit

---
+++ src/components/Blog/Blog.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Blog = ({blog}) => {
+    console.log(blog);
+    return (
+        <div>
+            <h1>This is Blog</h1>
+        </div>
+    );
+};
+
+export default Blog;
